f4a0a2e127c07fec090138ddacdb8cadab36b2db
soneye omojuwon <calebomojuwon@gmail.com>
2020-12-03 13:29:00 +0100

    done

---
+++ src/component/App.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react';
+import Editor from "./Editor"
+import useLocalStorage from '../hooks/useLocalStorage'
+
+function App() {
+  const [html,setHtml]=  useLocalStorage('html', '')
+  const [css,setCss]=  useLocalStorage('css', '')
+  const [js,setJs]=  useLocalStorage('js', '')
+  const srcDoc = 
+  `<html>
+  <body> ${html}</body>
+  <style>${css}</style>
+  <script>${js}</script>
+  
+  </html>`
+  return (
+  <>
+  <div className="pane top-pane">
+    <Editor language ="xml"
+      displayName="HTML"
+      value = {html}
+      onChange={setHtml}
+    />
+    <Editor 
+    language ="css"
+      displayName="CSS"
+      value = {css}
+      onChange={setCss}/>
+    <Editor
+      language ="css"
+      displayName="JS"
+      value = {js}
+      onChange={setJs}/>
+   
+  </div>
+  
+  
+  
+  
+  <div className="pane">
+  <iframe
+  srcDoc={srcDoc}
+    title="output"
+    sandbox="allow-scripts"
+    frameBorder="0"
+    width="100%"
+    height="100%"
+  />
+     
+  </div>
+  </>
+  );
+}
+
+export default App;
